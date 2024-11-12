e384387c7954fd6dd0dfbf0df05af2292892313e
linkcatlist <vigalok@bk.ru>
2024-11-12 17:46:20 +0300

    rational

---
+++ prj.lab/rational/rational.cpp
@@ -0,0 +1,139 @@
+#include "rational.hpp"
+
+#include <numeric>
+
+int Rational::Gcd(int a, int b) {
+    return std::gcd(a, b);
+}
+
+int Rational::Lcm(int a, int b) {
+    return a * b / Gcd(a, b);
+}
+
+void Rational::Normalize() {
+    int gcd = Gcd(Numerator, Denumerator);
+    Numerator /= gcd;
+    Denumerator /= gcd;
+    if (Denumerator < 0) {
+        Numerator *= (-1);
+        Denumerator *= (-1);
+    }
+}
+
+bool Rational::operator ==(const Rational& otherRational) const {
+    return Numerator == otherRational.Numerator && Denumerator == otherRational .Denumerator;
+}
+
+bool Rational::operator !=(const Rational& otherRational) const {
+    return !(*this == otherRational);
+}
+
+bool Rational::operator <(const Rational& otherRational) const {
+    int lcm = Lcm(Denumerator, otherRational.Denumerator);
+    return Numerator * (lcm / Denumerator) < otherRational.Numerator * (lcm / otherRational.Denumerator);
+}
+
+bool Rational::operator >(const Rational& otherRational) const {
+    return otherRational < *this;
+}
+
+bool Rational::operator <=(const Rational& otherRational) const {
+    return *this < otherRational || *this == otherRational;
+}
+
+bool Rational::operator >=(const Rational& otherRational) const {
+    return *this > otherRational || *this == otherRational;
+}
+
+Rational& Rational::operator +=(const Rational& otherRational) {
+    int lcm = Lcm(Denumerator, otherRational.Denumerator);
+    Numerator *= (lcm / Denumerator);
+    Numerator += otherRational.Numerator * (lcm / otherRational.Denumerator);
+    Denumerator = lcm;
+    Normalize();
+    return *this;
+}
+
+Rational& Rational::operator -=(const Rational& otherRational) {
+    int lcm = Lcm(Denumerator, otherRational.Denumerator);
+    Numerator *= (lcm / Denumerator);
+    Numerator -= otherRational.Numerator * (lcm / otherRational.Denumerator);
+    Denumerator = lcm;
+    Normalize();
+    return *this;
+}
+
+Rational& Rational::operator *=(const Rational& otherRational) {
+    Numerator *= otherRational.Numerator;
+    Denumerator *= otherRational.Denumerator;
+    Normalize();
+    return *this;
+}
+
+Rational& Rational::operator /=(const Rational& otherRational) {
+    Numerator *= otherRational.Denumerator;
+    Denumerator *= otherRational.Numerator;
+    Normalize();
+    return *this;
+}
+
+Rational operator +(const Rational& lhs, const Rational& rhs) {
+    int lcm = Rational::Lcm(lhs.Denumerator, rhs.Denumerator);
+    Rational sumRational = lhs;
+    sumRational.Numerator *= (lcm / sumRational.Denumerator);
+    sumRational += rhs.Numerator * (lcm / rhs.Denumerator);
+    sumRational.Denumerator = lcm;
+    sumRational.Normalize();
+    return sumRational;
+}
+
+Rational operator -(const Rational& lhs, const Rational& rhs) {
+    int lcm = Rational::Lcm(lhs.Denumerator, rhs.Denumerator);
+    Rational difRational = lhs;
+    difRational.Numerator *= (lcm / difRational.Denumerator);
+    difRational -= rhs.Numerator * (lcm / rhs.Denumerator);
+    difRational.Denumerator = lcm;
+    difRational.Normalize();
+    return difRational;
+}
+
+Rational operator *(const Rational& lhs, const Rational& rhs) {
+    return Rational(lhs.Numerator * rhs.Numerator, lhs.Denumerator * rhs.Denumerator);
+}
+
+Rational operator /(const Rational& lhs, const Rational& rhs) {
+    return Rational(lhs.Numerator * rhs.Denumerator, lhs.Denumerator * rhs.Numerator);
+}
+
+std::ostream& Rational::WriteTo(std::ostream& ostrm) const {
+    ostrm << LeftBrace << Numerator << Separator << Denumerator << RightBrace;
+    return ostrm;
+}
+
+std::istream& Rational::ReadFrom(std::istream& istrm) {
+    char leftBrace = '{';
+    char separator = '/';
+    char rightBrace = '}';
+    Rational otherRational;
+
+    istrm >> leftBrace >> otherRational.Numerator >> separator >> otherRational.Denumerator >> rightBrace;
+    if (istrm.good()) {
+        if (leftBrace == LeftBrace && separator == Separator && rightBrace == RightBrace) {
+            Numerator = otherRational.Numerator;
+            Denumerator = otherRational.Denumerator;
+            Normalize();
+        }
+        else {
+            istrm.setstate(std::ios_base::failbit);
+        }
+    }
+    return istrm;
+}
+
+std::ostream& operator <<(std::ostream& ostrm, const Rational& c) {
+    return c.WriteTo(ostrm);
+}
+
+std::istream& operator >>(std::istream& istrm, Rational& c) {
+    return c.ReadFrom(istrm);
+}
---
+++ prj.lab/rational/rational.hpp
@@ -0,0 +1,68 @@
+#pragma once
+
+#include <ostream>
+#include <iostream>
+
+class Rational {
+public:
+    Rational()
+    : Numerator(0)
+    , Denumerator(1)
+    {}
+
+    Rational(int numerator, int denumerator = 1)
+    : Numerator(numerator)
+    , Denumerator(denumerator)
+    {
+        this->Normalize();
+    }
+
+    Rational(const Rational& otherRational) {
+        Numerator = otherRational.Numerator;
+        Denumerator = otherRational.Denumerator;
+        this->Normalize();
+    }
+
+public:
+    void Normalize();
+
+private:
+    static int Gcd(int lhs, int rhs);
+    static int Lcm(int a, int b);
+
+public:
+    bool operator ==(const Rational& otherRational) const;
+    bool operator !=(const Rational& otherRational) const;
+    bool operator <(const Rational& otherRational) const;
+    bool operator >(const Rational& otherRational) const;
+    bool operator <=(const Rational& otherRational) const;
+    bool operator >=(const Rational& otherRational) const;
+
+    Rational& operator +=(const Rational& otherRational);
+    Rational& operator -=(const Rational& otherRational);
+    Rational& operator /=(const Rational& otherRational);
+    Rational& operator *=(const Rational& otherRational);
+
+    friend Rational operator +(const Rational& lhs, const Rational& rhs);
+    friend Rational operator -(const Rational& lhs, const Rational& rhs);
+    friend Rational operator *(const Rational& lhs, const Rational& rhs);
+    friend Rational operator /(const Rational& lhs, const Rational& rhs);
+
+public:
+    std::ostream& WriteTo(std::ostream& ostrm) const;
+    std::istream& ReadFrom(std::istream& istrm);
+
+public:
+    friend std::ostream& operator <<(std::ostream& ostrm, const Rational& c);
+    friend std::istream& operator >>(std::istream& istrm, Rational& c);
+
+private:
+    int Numerator = 0;
+    int Denumerator = 1;
+
+public:
+    static const char LeftBrace = '{';
+    static const char Separator = '/';
+    static const char RightBrace = '}';
+
+};
